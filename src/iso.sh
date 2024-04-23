b4c3b56e263b6eeb6b07338ddc31cdf2792f2ffd
raphaelabrantes <antenabr2@gmail.com>
2024-04-23 21:16:32 +0800

    Changing name

--- src/iso.sh
+++ src/iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/XiWangOS.kernel isodir/boot/XiWangOS.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "XiWangOS" {
+	multiboot /boot/XiWangOS.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o XiWangOS.iso isodir
